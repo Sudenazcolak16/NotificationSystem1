fc17eb77817b673b20b58637670a2b19bf5f24dc
Sudenazcolak16 <colaksudenaz6@gmail.com>
2026-05-08 16:14:06 +0300

    feat: bildirim günlüğü (logging) için Decorator deseni uygulandı

---
+++ src/LogDecorator.java
@@ -0,0 +1,19 @@
+package ödev1;
+
+
+public class LogDecorator extends NotificationDecorator {
+ 
+ public LogDecorator(Notification notification) {
+     super(notification);
+ }
+
+ @Override
+ public void sendMessage(String message, String target) {
+     
+     String timestamp = java.time.LocalDateTime.now().toString();
+     String loggedMessage = "[LOG - " + timestamp + "] " + message;
+     
+
+     super.sendMessage(loggedMessage, target);
+ }
+}
