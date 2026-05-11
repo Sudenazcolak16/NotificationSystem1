3b59416881b256885e9af04d5f9f72f5b6bdeb2f
Sudenazcolak16 <colaksudenaz6@gmail.com>
2026-05-11 13:57:11 +0300

    Delete src/ModernSmsLib.java

--- src/ModernSmsLib.java
+++
@@ -1,8 +0,0 @@
-package src;
-
-
-public class ModernSmsLib {
- public void sendFastMessage(String msg, int phoneNumber) {
-     System.out.println("ModernLib ile SMS: " + phoneNumber + " -> " + msg);
- }
-}
